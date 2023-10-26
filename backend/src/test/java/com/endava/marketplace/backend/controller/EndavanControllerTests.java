b5b60a2192cc2ee94369b9045f63390c07e40be1
Jhon Edward Mora <Jhon-Edward.Mora@endava.com>
2023-10-26 18:29:33 +0100

    #63: Hotfix: Solved certificate location environment variable issue
    Added a definition of an environment variable in the BE service configuration file, which would allow the service to load the certificate file without directly changing the application properties file.
    
    Squashed commit of the following:
    
    commit 51df88248be1185a115f05a5db10ddcc855698c7
    Author: Jhon Edward Mora <Jhon-Edward.Mora@endava.com>
    Date:   Thu Oct 26 12:16:54 2023 -0500
    
        Added environment variable definition in BE service file
    
    commit c0ae45bfb27ec19abd025c9b259f1c8066de2a85
    Author: Jhon Edward Mora <Jhon-Edward.Mora@endava.com>
    Date:   Thu Oct 26 12:15:37 2023 -0500
    
        Removed unnecesary colon

--- backend/src/test/java/com/endava/marketplace/backend/controller/EndavanControllerTests.java
+++ backend/src/test/java/com/endava/marketplace/backend/controller/EndavanControllerTests.java
@@ -4,7 +4,7 @@ import com.endava.marketplace.backend.model.Endavan;
 import com.endava.marketplace.backend.service.EndavanService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.CoreMatchers;
-import org.junit.jupiter.api.Test;;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
