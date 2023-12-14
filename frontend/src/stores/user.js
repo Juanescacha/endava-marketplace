83e6315fc0c0598d2d1cc0ee52fa659d0e083582
Juan Esteban Camargo <juanescacha@hotmail.com>
2023-12-14 18:18:46 -0500

    feat: :sparkles: default image for profile picture

--- frontend/src/stores/user.js
+++ frontend/src/stores/user.js
@@ -5,7 +5,7 @@ export const useUserStore = defineStore("user", {
 		id: 0,
 		name: "",
 		email: "",
-		image: "",
+		image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Windows_10_Default_Profile_Picture.svg/2048px-Windows_10_Default_Profile_Picture.svg.png",
 		isAdmin: false,
 	}),
 });
